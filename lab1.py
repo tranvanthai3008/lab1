a3789b71c816b70b8de61cc1016134a6f78096af
tranvanthai3008 <90456353+tranvanthai3008@users.noreply.github.com>
2021-12-29 22:51:22 +0300

    Add files via upload

--- lab1.py
+++ lab1.py
@@ -1,44 +1,53 @@
-import json
-# question 1 and 2
-char_total = 0 # q1
-char_total_format = 0 # q2
-text_data  = open("pokemon_full.json", "r")
-for line in text_data:
-	string_line = str(line)
-	# print(len(string_line))
-	char_total += len(string_line)
-	char_total_format += len(string_line) - string_line.count(" ") - string_line.count(".")
+"""
+A module show nnformation about the number of characters, characters without \
+spaces and punctuation marks, the name of the Pokemon, the name of the skills
+"""
 
-print("character total: "+str(char_total))
-print("character total after striming \".\" and \" \": " + str(char_total_format))
+import json
+
+# question 1 and 2
+CHAR_TOTAL = 0  # q1
+CHAR_TOTAL_FORMAT = 0  # q2
+
+file_data = open("pokemon_full.json")
+text_data = file_data.readlines()
+
+file_data.seek(0)
+pokemon_data = json.load(file_data)
+for line in text_data:
+    string_line = line
+    CHAR_TOTAL += len(string_line)
+    CHAR_TOTAL_FORMAT += (
+        len(string_line) - string_line.count(" ") - string_line.count(".")
+    )
+
+print(f"character total: {CHAR_TOTAL}")
+print(f'character total after striming "." and " ": {CHAR_TOTAL_FORMAT}')
 
 # question 3
-file_data = open("pokemon_full.json" , "r")
-pokemon_data = json.load(file_data) # type of data is list (1 dim array )
-max_of_len = 0
-index = 0
+MAX_LEN = 0
+INDEX = 0
 for i, pokemon in enumerate(pokemon_data):
-	if pokemon["description"] != "Description not available yet":
-		if len(pokemon["description"]) > max_of_len:
-			max_of_len = len(pokemon["description"])
-			index = i
-		
-print(max_of_len)		
-print("name: "+pokemon_data[index]["name"]+ "\nindex: "+str(index))
-print(pokemon_data[index])
+    if pokemon["description"] != "Description not available yet":
+        if len(pokemon["description"]) > MAX_LEN:
+            MAX_LEN = len(pokemon["description"])
+            INDEX = i
 
-#question 4
+name_max = pokemon_data[INDEX]["name"]
+print(f"Name: {name_max} \nIndex: {INDEX}")
+print(pokemon_data[INDEX])
 
-file_data = open("pokemon_full.json" , "r")
-pokemon_data = json.load(file_data) # type of data is list (1 dim array )
-max_of_len = 0
-index = 0
+# question 4
+MAX_ABILITIY = 0
+INDEX = 0
 for i, pokemon in enumerate(pokemon_data):
-	if pokemon["abilities"] != "abilities not available yet":
-		if len(pokemon["abilities"]) > max_of_len:
-			max_of_len = len(pokemon["abilities"])
-			index = i
-		
-print(max_of_len)		
-print("name: "+pokemon_data[index]["name"]+ "\nindex: "+str(index))
-print(pokemon_data[index])
+    if pokemon["abilities"] != "abilities not available yet":
+        if len(pokemon["abilities"]) > MAX_ABILITIY:
+            MAX_ABILITIY = len(pokemon["abilities"])
+            INDEX = i
+
+ability_name = pokemon_data[INDEX]["name"]
+print(f"Ability name {ability_name}")
+print(pokemon_data[INDEX])
+
+file_data.close()
